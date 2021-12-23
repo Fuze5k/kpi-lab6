6e481259103e3219dcf15437684d408d1d2368ef
Ярослав Григоренко <51718249+Fuze5k@users.noreply.github.com>
2021-12-23 07:05:21 +0200

    Add files via upload

---
+++ src/test/scala/simulations/GetBTCUSDTInfoApiSimulation.scala
@@ -0,0 +1,29 @@
+package simulations
+import io.gatling.core.scenario.Simulation
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class GetBTCUSDTInfoApiSimulation extends Simulation {
+
+  //http conf
+
+  var httpConf =  http.baseUrl("https://api.binance.com/api")
+    .header("Accept", "application/json")
+    .header("content-type", "application/json")
+
+  val sentHeaders = Map(
+    "Content-Type" -> "application/javascript",
+    "Accept" -> "text/html"
+  )
+
+  //scenario
+
+  var scen = scenario("GetBySymbol")
+    .exec(http("GetBTCUSDT")
+      .get("/v3/ticker/24hr?symbol=BTCUSDT")
+      .headers(sentHeaders)
+      .check(status is 200))
+
+  // set up
+  setUp(scen.inject(atOnceUsers(15))).protocols(httpConf)
+}
